31f8826c1d676d2eb8c116b627289eeea9e44199
kpk09 <kpk09@biomedia07.doc.ic.ac.uk>
2014-07-16 19:11:06 +0100

    saturated thumbnails

--- show.py
+++ show.py
@@ -4,16 +4,18 @@ import irtk
 from glob import glob
 import os
 
-def show(all_files,prefix=""):
+def show(all_files,prefix="",saturate=False):
     for f in all_files:
         name = os.path.basename(f)[:-len('.nii.gz')]
         img = irtk.imread(f,dtype="float32")
+        if saturate:
+            img = img.saturate().rescale()
         png_name = "img/"+prefix+name+".png"
         print png_name
         irtk.imshow(img,filename=png_name)
     
-show(glob("data/*.nii.gz"))
+show(glob("data/*.nii.gz"),saturate=True)
 show(glob("output_detection/*.nii.gz"),"detection_")
-show(glob("output_segmentation/*.nii.gz"))
-show(["output_reconstruction/motion_corrected_volume.nii.gz"])
+show(glob("output_segmentation/*.nii.gz"),saturate=True)
+show(["output_reconstruction/motion_corrected_volume.nii.gz"],saturate=True)
 
